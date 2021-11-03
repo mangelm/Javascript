31a07716f0b9b011d6cf613f7f3e7901babae12f
mangelm <miguelangelmorenodelgadot@gmail.com>
2021-11-03 11:01:06 +0100

    definicion variables

---
+++ Grover Vasquez/01-Fundamentos/app.js
@@ -0,0 +1,24 @@
+/**************************
+ * Lección de definición de Variables
+ **************************/
+
+//console.log("Hola a todo!!");
+
+var primerNombre = 'Angel'; //Variable tipo String 
+console.log(primerNombre);
+
+var edad = 34;  //Tipo numerica
+var sueldo = 1800.99;
+
+console.log(edad);
+
+var tieneTrabajo = true; //Tipo boolean
+console.log(tieneTrabajo);
+
+/* var puestoDeTrabajo; //Indefinida
+console.log(puestoDeTrabajo);
+
+tieneTrabajo = null; //Reasignando nuevo valor 
+console.log(tieneTrabajo); */
+
+var cajas;
