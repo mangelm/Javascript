af3bb5d645fdea3a1fd81f1f7e6271ce12e1d49a
mangelm <miguelangelmorenodelgadot@gmail.com>
2021-11-21 11:49:41 +0100

    Seccion 5 v1

---
+++ Grover Vasquez/04-Funciones/app.js
@@ -0,0 +1,12 @@
+/* function bienvenido(){
+    console.log('Hola Bienvenido a la sección de funciones');
+}
+
+bienvenido();
+ */
+function bienvenido(){
+    return 'Hola Bienvenido a la sección de funciones';
+}
+
+var mensaje = bienvenido();
+console.log(mensaje); 
