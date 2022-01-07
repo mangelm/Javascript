67e7b8f36852a5246b925dfca3095903682648fc
mangelm <miguelangelmorenodelgadot@gmail.com>
2022-01-07 06:36:14 +0100

    s9 v1

---
+++ V2/08-Objetos/app.js
@@ -0,0 +1,35 @@
+/******************************************************
+                Objetos - Lo básico
+********************************************************/
+
+/* let miLibro = {
+    titulo: 'El libro de JavasScript',
+    autor: 'Angel Moreno',
+    paginas: 400,
+    publicado: false
+} */
+
+// console.log(miLibro);
+// console.log(miLibro.titulo);
+// console.log(miLibro.autor);
+
+// console.log(`${miLibro.titulo} creado por ${miLibro.autor}`);
+// miLibro.paginas = 500;
+// console.log(miLibro.paginas);
+
+/******************************************************
+                Ejercicio codificacion
+********************************************************/
+
+//Ejercicio, crear un objeto persona que tenga las siguientes prop:
+//nombre,edad,ciudad
+//cambiar la edad
+//Mostrar datos de la persona
+
+/******************************************************
+                 MI VERSION
+********************************************************/
+
+/******************************************************
+                VERSION GROVER
+********************************************************/
